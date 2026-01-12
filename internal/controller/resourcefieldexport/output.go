2bdde43164c10c1396fade7c02edc78a7013a961
Jithu M Kunjuraman <jithu.kunjuraman@deliveryhero.com>
2026-01-12 16:14:16 +0100

    fix: improve errors and improve filtering (#65)
    * fix: move ci to GHCR
    
    * fix version
    
    * fix lint
    
    * fix codeowners
    
    * test release
    
    * fix tag
    
    * test
    
    * fix: improve errors and improve filtering
    
    * fix release json
    
    * fix controllergen version

--- internal/controller/resourcefieldexport/output.go
+++ internal/controller/resourcefieldexport/output.go
@@ -5,13 +5,18 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 func (r *Reconciler) writeToSecret(ctx context.Context, name string, namespace string, values map[string]string) error {
+	logger := log.FromContext(ctx)
 	var targetSecret v1.Secret
 	err := r.Get(ctx, client.ObjectKey{Name: name, Namespace: namespace}, &targetSecret)
 	if err != nil {
 		// todo: disambiguate on type of error
+		logger.Error(err, "failed to get target Secret",
+			"name", name,
+			"namespace", namespace)
 		return err
 	}
 	secretCopy := targetSecret.DeepCopy()
@@ -21,14 +26,30 @@ func (r *Reconciler) writeToSecret(ctx context.Context, name string, namespace s
 	for k, v := range values {
 		secretCopy.Data[k] = []byte(v)
 	}
-	return r.Update(ctx, secretCopy)
+	err = r.Update(ctx, secretCopy)
+	if err != nil {
+		logger.Error(err, "failed to update Secret",
+			"name", name,
+			"namespace", namespace,
+			"keyCount", len(values))
+		return err
+	}
+	logger.Info("successfully updated Secret",
+		"name", name,
+		"namespace", namespace,
+		"keyCount", len(values))
+	return nil
 }
 
 func (r *Reconciler) writeToConfigMap(ctx context.Context, name string, namespace string, values map[string]string) error {
+	logger := log.FromContext(ctx)
 	var targetConfigMap v1.ConfigMap
 	err := r.Get(ctx, client.ObjectKey{Name: name, Namespace: namespace}, &targetConfigMap)
 	if err != nil {
 		// todo: disambiguate on type of error
+		logger.Error(err, "failed to get target ConfigMap",
+			"name", name,
+			"namespace", namespace)
 		return err
 	}
 	cmCopy := targetConfigMap.DeepCopy()
@@ -38,5 +59,17 @@ func (r *Reconciler) writeToConfigMap(ctx context.Context, name string, namespac
 	for k, v := range values {
 		cmCopy.Data[k] = v
 	}
-	return r.Update(ctx, cmCopy)
+	err = r.Update(ctx, cmCopy)
+	if err != nil {
+		logger.Error(err, "failed to update ConfigMap",
+			"name", name,
+			"namespace", namespace,
+			"keyCount", len(values))
+		return err
+	}
+	logger.Info("successfully updated ConfigMap",
+		"name", name,
+		"namespace", namespace,
+		"keyCount", len(values))
+	return nil
 }
